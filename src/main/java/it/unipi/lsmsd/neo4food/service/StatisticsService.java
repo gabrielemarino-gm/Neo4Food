a6d8ba20b949580f94e21ad26a7fe86181a9fcf4
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2023-01-20 17:42:43 +0100

    Delete file

--- src/main/java/it/unipi/lsmsd/neo4food/service/StatisticsService.java
+++
@@ -1,8 +0,0 @@
-package it.unipi.lsmsd.ecommerce.service;
-
-import it.unipi.lsmsd.ecommerce.dto.statistics.DashboardDTO;
-import it.unipi.lsmsd.ecommerce.service.exception.BusinessException;
-
-public interface StatisticsService {
-    public DashboardDTO getDashboardStatsDTO() throws BusinessException;
-}
