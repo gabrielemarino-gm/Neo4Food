413381899d7df20b669e4eb503726ebaee7c46cf
Matteo Pasqualetti <43815324+PatatoBy@users.noreply.github.com>
2023-02-02 12:19:39 +0100

    Add and see comments

--- src/main/java/it/unipi/lsmsd/neo4food/dao/neo4j/SocialNeoDAO.java
+++ src/main/java/it/unipi/lsmsd/neo4food/dao/neo4j/SocialNeoDAO.java
@@ -1,7 +1,13 @@
 package it.unipi.lsmsd.neo4food.dao.neo4j;
 
+import it.unipi.lsmsd.neo4food.dto.CommentDTO;
+import it.unipi.lsmsd.neo4food.dto.ListDTO;
+import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.neo4j.driver.Values.parameters;
 
@@ -25,41 +31,6 @@ public class SocialNeoDAO extends BaseNeo4J{
         }
     }
 
-    /** Imposta un commento da parte di un utente verso un ristorante
-     *
-     *  Se un commento esiste gia, quello vecchio viene sovrascritto */
-
-    public void setRating(String user , String restaurant, String raddress, int rate) {
-        try (Session session = driver.session()) {
-
-            String searchQuery = "MATCH (u:User)-[rate:RATED]->(r:Restaurant) " +
-                                 "WHERE u.username = $user AND r.name = $restaurant AND r.address = $raddress " +
-                                 "RETURN r";
-
-            session.writeTransaction(tx -> {
-//      Controllo se commento esiste gia
-                Result result = tx.run(searchQuery, parameters("user", user, "restaurant", restaurant ,"raddress" ,raddress ));
-
-//      Se esiste già, aggiorna il rating
-                if(result.hasNext()) {
-                    String modifyQuery = "MATCH (u:User)-[rate:RATED]->(r:Restaurant) " +
-                                         "WHERE u.username = $user AND r.name = $restaurant AND r.address = $raddress " +
-                                         "SET rate.rating = $rating";
-
-                    tx.run(modifyQuery, parameters("user", user, "restaurant", restaurant, "raddress", raddress, "rating", rate)).consume();
-
-                }
-//      Se non esiste, aggiungo nuovo rating
-                else {
-                    String setQuery = "MATCH (u:User) ,(r:Restaurant) " +
-                                      "WHERE u.username = $user AND r.name = $restaurant AND r.address = $raddress " +
-                                      "MERGE (u)-[:RATED{rating: $rating}]->(r)";
-                    tx.run(setQuery, parameters("user", user, "restaurant", restaurant, "raddress", raddress, "rating", rate)).consume();
-                }
-                return 1;
-            });
-        }
-    }
 
     /** Esegue una raccomandazione ad un utente di 10 ristoranti
      *
@@ -68,7 +39,7 @@ public class SocialNeoDAO extends BaseNeo4J{
     public void getRecommendation(String user, String zipcode) {
         try (Session session = driver.session()) {
             String searchQuery = "MATCH (u1:User)-[:IS_FRIEND]->(u2:User)-[rate:RATED]->(r:Restaurant) " +
-                                 "WHERE u1.username = $user and r.zipcdoe = $zipcode" +
+                                 "WHERE u1.username = $user and r.zipcode = $zipcode" +
                                  "WITH r, COUNT(rate.rating) as nrating, " +
                                  "AVG(rate.rating) as avg_rating " +
                                  "ORDER BY nrating, avg_rating DESC " +
@@ -82,6 +53,82 @@ public class SocialNeoDAO extends BaseNeo4J{
                 return 1;
             });
         }
+    }
+
+    /** Imposta un commento da parte di un utente verso un ristorante
+     *
+     *  Se un commento esiste gia, quello vecchio viene sovrascritto
+     *
+     *  INPUT - ReastaurantId, username utente, rating, review testuale */
+    public void setRating(String user , String rid, int rate, String review) {
+        try (Session session = driver.session()) {
+
+
+            String searchQuery = "MATCH (u:User)-[rate:RATED]->(r:Restaurant) " +
+                                 "WHERE u.username = $user AND r.rid = $rid " +
+                                 "RETURN r";
+
+            session.writeTransaction(tx -> {
+//      Controllo se commento esiste gia
+                Result result = tx.run(searchQuery, parameters("user", user, "rid", rid));
+
+//      Se esiste già, aggiorna il rating
+                if(result.hasNext()) {
+                    String modifyQuery = "MATCH (u:User)-[rate:RATED]->(r:Restaurant) " +
+                                         "WHERE u.username = $user AND r.rid = $rid " +
+                                         "SET rate.rating = $rating, rate.review = $review";
+
+                    tx.run(modifyQuery, parameters("user", user, "rid", rid, "rating", rate, "review", review)).consume();
+
+                }
+//      Se non esiste, aggiungo nuovo rating
+                else {
+                    String setQuery = "MATCH (u:User) ,(r:Restaurant) " +
+                                      "WHERE u.username = $user AND r.rid = $rid " +
+                                      "MERGE (u)-[:RATED{rating: $rating, review: $review}]->(r)";
+
+                    tx.run(setQuery, parameters("user", user, "rid", rid, "rating", rate, "review", review)).consume();
+                }
+                return 1;
+            });
+        }
+    }
+
+    /** Ritorna una lista di 20 commenti del ristorante richiest
+     *
+     *  INPUT - restaurantId, pagina di commenti */
+    public ListDTO<CommentDTO> getComments(String restaurantid, int page){
+
+        try (Session session = driver.session()) {
+            String searchQuery = "MATCH (r:Restaurant)<-[rate:RATED]-(u:User) " +
+                    "WHERE r.rid = $rid " +
+                    "RETURN u.name as user, rate.rating as rate, rate.review as comment " +
+                    "SKIP $skip " +
+                    "LIMIT 20";
+
+            session.writeTransaction(tx -> {
+                Result result = tx.run(searchQuery, parameters( "rid", restaurantid, "skip", (page*20)));
+                ListDTO<CommentDTO> toReturn = new ListDTO<CommentDTO>();
+                List<CommentDTO> tempList = new ArrayList<CommentDTO>();
+
+                while(result.hasNext()){
+                    Record r = result.next();
+                    CommentDTO tempComment = new CommentDTO();
+
+                    tempComment.setUserName(r.get("user").asString());
+                    tempComment.setRate(r.get("rate").asDouble());
+                    tempComment.setCommentText(r.get("comment")!= null ? r.get("comment").asString() : "");
+
+                    tempList.add(tempComment);
+                }
+
+                toReturn.setList(tempList);
+                toReturn.setItemCount(tempList.size());
+
+                return toReturn;
+            });
+        }
+        return null;
     }
 
     @Override
---
+++ src/main/java/it/unipi/lsmsd/neo4food/servlet/Social.java
@@ -0,0 +1,55 @@
+package it.unipi.lsmsd.neo4food.servlet;
+
+import com.google.gson.Gson;
+import it.unipi.lsmsd.neo4food.dto.CommentDTO;
+import it.unipi.lsmsd.neo4food.dto.ListDTO;
+import it.unipi.lsmsd.neo4food.service.ServiceProvider;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@WebServlet("/social")
+public class Social extends HttpServlet
+{
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        String targetJSP = "WEB-INF/jsp/restaurant.jsp";
+        String actionType = request.getParameter("action");
+
+        if(actionType.equals("getComments")){
+            String rid = request.getParameter("restaurantId");
+            int page = Integer.parseInt(request.getParameter("page"));
+
+            ListDTO<CommentDTO> commentList = ServiceProvider.getSocialService().getComments(rid, page);
+
+            String toSend = new Gson().toJson(commentList);
+
+            if(commentList != null){
+                response.getWriter().println(toSend);
+                response.getWriter().flush();
+            }
+            else
+            {
+                response.getWriter().println("{intCount : 0}");
+                response.getWriter().flush();
+            }
+
+            return;
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(targetJSP);
+        dispatcher.forward(request, response);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        doRequest(request, response);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        doRequest(request, response);
+    }
+
+
+}
